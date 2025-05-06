20228d9763df41127697e8293d8834732c6f2d4a
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-05-06 15:47:08 +0000

    my programe on Sccanner

--- details.java
+++ details.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+class details{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = input.nextLine();
+        System.out.println("Enter your fathers name:");
+        String fname = input.nextLine();
+        System.out.println("Enter your mothers name :");
+        String mname = input.nextLine();
+        System.out.println("Enter your current age");
+        int age = input.nextInt();
+        System.out.println("Enter your tenth calss percentage:");
+        int page = input.nextInt();
+        System.out.println("My name is:"+name);
+        System.out.println("My fathers name is :"+fname);
+        System.out.println("My mothers name is :"+mname);
+        System.err.println("My age is :"+age);
+        System.err.println("My percentage is :"+page);
+
+
+
+    }
+}
