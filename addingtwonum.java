2ddf8895d2a6feb8563908812ea6570ff4b9668f
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-05-06 14:46:34 +0000

    m

---
+++ addingtwonum.java
@@ -0,0 +1,8 @@
+class addingtwonum {
+    public static void main(String arga[]){
+        int a = 6;
+        int b = 8;
+        int c = a+b;
+        System.out.println("The sum of two numbers is"+c);
+    }
+}
